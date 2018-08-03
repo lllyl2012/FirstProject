f0d5b6db2a2287db0fb346a56ce63dd88ea88de5
kkczx <313102983@qq.com>
2018-08-03 14:57:03 +0800

    2018.8.3更新： 把service和serviceImpl做一下调整

--- src/main/java/com/hr/service/MemberService.java
+++ src/main/java/com/hr/service/MemberService.java
@@ -1,19 +1,11 @@
 package com.hr.service;
 
-import com.hr.service.impl.MemberServiceImpl;
 
-public class MemberService implements MemberServiceImpl{
-
-	public boolean checkHaveUser(String username) {
-		try {
-			//执行持久层的方法,根据用户名查询，如果能查询到结果，就返回true
-			//如果不能查询到,就抛出异常,并被catch到返回false
-			//这里以后写持久层方法
-			return true;
-		}catch(Exception e) {
-			return false;
-		}
-	}
-	
-
+public interface MemberService {
+	/**
+	 * 检查用户名是否存在
+	 * @param username
+	 * @return
+	 */
+	public boolean checkHaveUser(String username);
 }
--- src/main/java/com/hr/service/impl/CompanyInfoServiceImpl.java
+++ src/main/java/com/hr/service/impl/CompanyInfoServiceImpl.java
@@ -1,9 +1,21 @@
 package com.hr.service.impl;
 
-public interface CompanyInfoServiceImpl {
+import org.springframework.stereotype.Service;
 
+import com.hr.service.CompanyInfoService;
+@Service
+public class CompanyInfoServiceImpl implements CompanyInfoService{
 	/**
 	 * 检查公司信息是否完整
 	 */
-	public boolean checkResumeFull();
+	public boolean checkResumeFull() {
+		try {
+			//执行持久层的方法,如果数据所有库字段都非空,就返回true
+			//如果有字段是空,就抛出异常,并被catch到返回false
+			//这里以后写持久层方法
+			return true;
+		}catch(Exception e) {
+			return false;
+		}
+	}
 }
