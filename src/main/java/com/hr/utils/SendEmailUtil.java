349f46ac454dc2ffdbc090f696ae56bc4c2e9ff1
kkczx <313102983@qq.com>
2018-08-05 19:57:55 +0800

    添加了发送邮件的工具类

---
+++ src/main/java/com/hr/utils/SendEmailUtil.java
@@ -0,0 +1,28 @@
+package com.hr.utils;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SendEmailUtil {
+	
+	@Autowired
+	JavaMailSender jms;
+	public String sendEmail(){
+		//建立邮件消息
+		SimpleMailMessage mainMessage = new SimpleMailMessage();
+		//发送者
+		mainMessage.setFrom("xxxxx@163.com");
+		//接收者
+		mainMessage.setTo("xxxx@qq.com");
+		//发送的标题
+		mainMessage.setSubject("嗨喽");
+		//发送的内容
+		mainMessage.setText("hello world");
+		jms.send(mainMessage);
+		return "send ok";
+	}
+
+}
